a8b4ee387c524f6f8eb7fce9af600520fa0d12bd
elinagomez <45207375+elinagomez@users.noreply.github.com>
2021-05-28 11:48:27 -0300

    Mejoras, compila y nubes

--- shinyApp_speech.R
+++ shinyApp_speech.R
@@ -8,6 +8,11 @@ library(shinycssloaders)
 library(xlsx)
 library(quanteda)
 library(kableExtra)
+library(wordcloud2)
+library(dplyr)
+library(bslib)
+library(bsplus)
+
 
 shinyAppUI = fluidPage(
   # define theme ####
@@ -21,7 +26,15 @@ shinyAppUI = fluidPage(
              windowTitle = "speech App"),
   
   textAreaInput("url", "Ingrese URL", "",width = '800px' ),
-  hr(), 
+  h6("* Es posible ingresar más de una URL, separando con coma (,)"),
+  hr(),
+  div(checkboxInput(
+    inputId = "compila",
+    label = "Compilar", FALSE,width = "13%") %>%
+    shinyInput_label_embed(
+      shiny_iconlink() %>%
+        bs_embed_popover(title = "Compilar", content = "Se debe marcar la opción Compilar si se quiere agrupar todas las intervenciones de cada uno de los legisladores en un documento o conjunto de documentos.", placement = "bottom")),use_bs_popover()),
+  br(),
   actionButton("boton", "Buscar",class="btn btn-primary"),
   downloadButton('descargar', 'Descargar datos (.xlsx)',class = "btn btn-primary"), 
   hr(), 
@@ -30,11 +43,10 @@ shinyAppUI = fluidPage(
             tabsetPanel(tabPanel(div("Tabla"),
                                  wellPanel(h3(""),
                                            h5(""),
-                                           fluidRow(column(12, htmlOutput("resumen")
-                                           ),))),  tabPanel(div("Nubes de palabras"),
+                                           fluidRow(column(12, DTOutput("resumen")%>% withSpinner(color="#36a1dc") ),))),  tabPanel(div("Nubes de palabras"),
                                                             wellPanel(h3(""),
                                                                       h5(""),
-                                                                      fluidRow(column(12, imageOutput("nube")
+                                                                      fluidRow(column(12, wordcloud2Output("nube",width = "100%")
                                                                       ),))))
             
             
@@ -44,17 +56,37 @@ shinyAppUI = fluidPage(
 
 shinyAppServer <- function(input, output) {
   
-  d <- reactive({
-    speech::speech_build(input$url,compiler = TRUE) 
-  })
   
-  observeEvent(input$boton,{ output$resumen <- renderText({
-    d() %>%
-      as.data.frame()%>%
-      knitr::kable("html")%>%
-      kable_styling("striped", full_width = F)
+
+  
+  
+  d <- eventReactive(input$boton,{
+    
+    
+    urls <- unlist(strsplit(input$url, ","))
+    
+    urls=c(paste0(urls, "_", seq_along(urls)))
+    
+    speech::speech_build(urls,compiler = input$compila) 
     
   })
+  
+  
+
+    output$resumen <- renderDT({
+    
+      
+      datatable(
+        d(),
+        rownames = TRUE,
+        #extensions = 'Buttons',
+        options = list(
+          pageLength = 50,
+          dom = 'Bfrtip'))
+    
+  })
+    
+    
   
   output$descargar <- downloadHandler(
     filename = function() {
@@ -65,30 +97,60 @@ shinyAppServer <- function(input, output) {
     }
   )
   
+  output$nube <- renderWordcloud2({
+    
+      
+      d()%>%
+          as.data.frame()%>%
+          quanteda::corpus(.,text_field = "speech") %>%
+          quanteda::dfm(.,stem = FALSE,
+                        tolower = TRUE,
+                        remove = c(stopwords("spanish"),"señor", "señora","legislador","legisladora"),
+                        remove_punct = TRUE,
+                        remove_numbers = TRUE,
+                        verbose = FALSE)%>%
+          dfm_remove(min_nchar=3)%>%
+      dfm_trim(min_termfreq = 2)%>%
+      quanteda.textstats::textstat_frequency()%>%
+           as.data.frame()%>%select(feature ,frequency)%>%
+      dplyr::rename(word=feature,freq=frequency)%>%
+      wordcloud2(size=0.9,color= rev(RColorBrewer::brewer.pal(9,"Blues")),backgroundColor = "grey")
+      
+      
+    
   })
   
   
-  output$nube <- renderImage({
-    salida <- tempfile(fileext='.png')
-    png(salida, width=500, height=500)
-    d()%>%
-      as.data.frame()%>%
-      quanteda::corpus(.,text_field = "speech") %>%
-      quanteda::dfm(.,stem = FALSE,
-                    tolower = TRUE,
-                    remove = c(stopwords("spanish"),"señor", "señora","legislador","legisladora"), 
-                    remove_punct = TRUE, 
-                    remove_numbers = TRUE, 
-                    verbose = FALSE)%>%
-      dfm_remove(min_nchar=3)  %>%
-      textplot_wordcloud(., min.count = 3,max_words = 150,random.order = FALSE,
-                         rot.per = .50, colors = RColorBrewer::brewer.pal(8,"Dark2"))
-    dev.off()
-    
-    # Return a list
-    list(src = salida,
-         alt = "Texto")
-  }, deleteFile = TRUE)
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  # output$nube <- renderImage({
+  #   salida <- tempfile(fileext='.png')
+  #   png(salida, width=500, height=500)
+  #   d()%>%
+  #     as.data.frame()%>%
+  #     quanteda::corpus(.,text_field = "speech") %>%
+  #     quanteda::dfm(.,stem = FALSE,
+  #                   tolower = TRUE,
+  #                   remove = c(stopwords("spanish"),"señor", "señora","legislador","legisladora"), 
+  #                   remove_punct = TRUE, 
+  #                   remove_numbers = TRUE, 
+  #                   verbose = FALSE)%>%
+  #     dfm_remove(min_nchar=3)  %>%
+  #     textplot_wordcloud(., min.count = 3,max_words = 150,random.order = FALSE,
+  #                        rot.per = .50, colors = RColorBrewer::brewer.pal(8,"Dark2"))
+  #   dev.off()
+  #   
+  #   # Return a list
+  #   list(src = salida,
+  #        alt = "Texto")
+  # }, deleteFile = TRUE)
   
 }
 
